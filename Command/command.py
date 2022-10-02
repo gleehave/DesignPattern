2f5b7090c618a51fc37b43be47197fa76a7331c3
leegangho <gleehave@gmail.com>
2022-10-02 12:28:00 +0900

    [add] Command - Stock

--- Command/command.py
+++ Command/command.py
@@ -1,3 +1,6 @@
+from abc import ABCMeta, abstractmethod
+
+
 class Wizard():
     def __init__(self, src, rootdir):
         self.choices = []
@@ -14,10 +17,56 @@ class Wizard():
             else:
                 print("No Operation")
     
+
+class Order(metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self):
+        pass
+
+class BuyStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+    
+    def execute(self):
+        self.stock.buy()
+
+class SellStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+    
+    def execute(self):
+        self.stock.sell()
+
+class StockTrade:
+    def buy(self):
+        print("You will buy stocks")
+    
+    def sell(self):
+        print("You will sell stocks")
+
+class Agent:
+    def __init__(self):
+        self.__orderQueue = []
+    
+    def placeOrder(self, order):
+        self.__orderQueue.append(order)
+        order.execute()
+
 if __name__ == "__main__":
-    # 클라이언트
+    # Client
     wizard = Wizard('python.3.5. gzip', '/usr/bin/')
     
     wizard.preferences({"python": True})
     wizard.preferences({"java":False})
     wizard.execute()
+    
+    
+    # Client
+    stock = StockTrade()
+    buyStock = BuyStockOrder(stock)
+    sellStock = SellStockOrder(stock)
+    
+    # Inovker
+    agent = Agent()
+    agent.placeOrder(buyStock)
+    agent.placeOrder(sellStock)
