c7a45e00455f84c32c86313132282ce8ff7d9a63
gleehave <gleehave@gmail.com>
2022-12-20 12:52:50 +0900

    [add]  Stragety

---
+++ Strategy/Strategy.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+class Context():
+    def __init__(self, strategy: Strategy):
+        self._strategy = strategy
+
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy: Strategy):
+        self._strategy = strategy
+
+    def do_some_business_logic(self):
+        print("Context: Sorting data using the strategy (not sure how it'll do it)")
+        result = self._strategy.do_algorithm(['a','b','c','d','e'])
+        print(",".join(result))
+
+class Strategy(ABC):
+    @abstractmethod
+    def do_algorithm(self, data: List):
+        pass
+
+class ConcreteStrategyA(Strategy):
+    def do_algorithm(self, data: List):
+        return sorted(data)
+
+class ConcreteStrategyB(Strategy):
+    def do_algorithm(self, data: List):
+        return reversed(sorted(data))
+
+if __name__ == "__main__":
+    context = Context(ConcreteStrategyA())
+    print("Client: Strategy is set to normal sorting.")
+    context.do_some_business_logic()
+    print()
+
+    print("Client: Strategy is set to reverse sorting")
+    context.strategy = ConcreteStrategyB()
+    context.do_some_business_logic()
